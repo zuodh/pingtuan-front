73fb2f1cbf7304d5db7b6d3f50d6842c36b07433
zuodaohui <zdh_china@163.com>
2017-03-17 22:02:36 +0800

    生鲜拼团

---
+++ src/test/java/com/pingtuan/fresh/PingtuanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pingtuan.fresh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PingtuanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
